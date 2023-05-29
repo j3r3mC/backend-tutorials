cba7918483662b393ecd8f3e61d9916733271e2b
J3r3mC <j.corbeilla@outlook.fr>
2023-05-29 20:05:41 +0200

    Fix user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,22 +3,19 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    // Create a new Tutorial
+    // Create a new user
     router.post("/", users.create);
 
     // Retrieve all users
     router.get("/", users.findAll);
 
-    // Retrieve all published users
-    router.get("/published", users.findAllPublished);
-
-    // Retrieve a single Tutorial with id
+    // Retrieve a single User with id
     router.get("/:id", users.findOne);
 
-    // Update a Tutorial with id
+    // Update a User with id
     router.put("/:id", users.update);
 
-    // Delete a Tutorial with id
+    // Delete a User with id
     router.delete("/:id", users.delete);
 
     // Delete all users
